059fd349d534bd1f1969eba80e9679afe7199798
marcoshcj <marcoshcj@github.com>
2019-12-12 11:25:48 -0300

    12/12
    marcos

--- SenaiTCCdeusetop/app/src/main/java/com/example/senaitccdeusetop/Activitys/PesquisaActivity.java
+++ SenaiTCCdeusetop/app/src/main/java/com/example/senaitccdeusetop/Activitys/PesquisaActivity.java
@@ -8,8 +8,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.senaitccdeusetop.R;
+
+import java.lang.reflect.Array;
 
 public class PesquisaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
 
@@ -17,35 +20,41 @@ public class PesquisaActivity extends AppCompatActivity implements AdapterView.O
     Spinner spnDia,spnInicio,spnFim;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesquisa);
+
+
+
         btnPesquisar = findViewById(R.id.btnPesquisar);
-
-
         spnDia = findViewById(R.id.spnDia);
-        spnDia.setOnItemSelectedListener(this);
-        ArrayAdapter<CharSequence> adpDias = ArrayAdapter.createFromResource(this,
-                R.array.dias, android.R.layout.simple_spinner_item);
-        adpDias.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spnDia.setAdapter(adpDias);
+        spnInicio = findViewById(R.id.spnInicio);
+        spnFim = findViewById(R.id.spnFim);
 
+
+
+       ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.dias, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnDia.setAdapter(adapter);
+        spnDia.setOnItemSelectedListener(this);
 
 
         ArrayAdapter<CharSequence> adpHorario = ArrayAdapter.createFromResource(this,
                 R.array.Horario, android.R.layout.simple_spinner_item);
 
-        spnInicio = findViewById(R.id.spnInicio);
+
         spnInicio.setOnItemSelectedListener(this);
         adpHorario.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spnInicio.setAdapter(adpHorario);
+        spnInicio.setOnItemSelectedListener(this);
 
 
-        spnFim = findViewById(R.id.spnFim);
         spnFim.setOnItemSelectedListener(this);
         adpHorario.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spnFim.setAdapter(adpHorario);
+        spnFim.setOnItemSelectedListener(this);
 
 
 
@@ -55,6 +64,14 @@ public class PesquisaActivity extends AppCompatActivity implements AdapterView.O
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        switch(adapterView.getId()){
+            case 1:
+                Toast.makeText(this, "aquui", Toast.LENGTH_SHORT).show();
+
+        }
+
+
 
     }
 
