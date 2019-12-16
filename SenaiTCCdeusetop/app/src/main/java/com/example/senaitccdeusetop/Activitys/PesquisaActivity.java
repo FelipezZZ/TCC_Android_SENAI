48c7e7a3768b056ba76e86d5230f886db9ed68cc
caiocgalli <caiogalli2000@hotmail.com>
2019-12-16 09:52:51 -0200

    Revert "Merge branch 'master' of https://github.com/FelipezZZ/TCC_Android_SENAI"
    This reverts commit 8a97cc1f5d0bdfa48879a423870d1fc5df2e6b86, reversing
    changes made to 37990e3c5f5dd1dd73bbfa073c2c32016bf91577.

--- SenaiTCCdeusetop/app/src/main/java/com/example/senaitccdeusetop/Activitys/PesquisaActivity.java
+++ SenaiTCCdeusetop/app/src/main/java/com/example/senaitccdeusetop/Activitys/PesquisaActivity.java
@@ -24,11 +24,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-<<<<<<< HEAD
-import java.lang.reflect.Array;
-
-public class PesquisaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
-=======
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -38,7 +33,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class PesquisaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
->>>>>>> f274339633589b317b3330de1f350229ae3d8a82
 
     Button btnPesquisar;
     Spinner spnDia, spnInicio, spnFim;
@@ -48,13 +42,10 @@ public class PesquisaActivity extends AppCompatActivity implements AdapterView.O
     private String parametros;
     private String tipoUsuario;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesquisa);
-<<<<<<< HEAD
-=======
         btnPesquisar = findViewById(R.id.btnPesquisar);
         btnPesquisar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,36 +53,29 @@ public class PesquisaActivity extends AppCompatActivity implements AdapterView.O
                 Pesquisar(diaFormatado, inicio, fim);
             }
         });
->>>>>>> f274339633589b317b3330de1f350229ae3d8a82
 
 
-
-        btnPesquisar = findViewById(R.id.btnPesquisar);
         spnDia = findViewById(R.id.spnDia);
-        spnInicio = findViewById(R.id.spnInicio);
-        spnFim = findViewById(R.id.spnFim);
-
-
-
-       ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.dias, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spnDia.setAdapter(adapter);
         spnDia.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adpDias = ArrayAdapter.createFromResource(this,
+                R.array.dias, android.R.layout.simple_spinner_item);
+        adpDias.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnDia.setAdapter(adpDias);
+
 
         ArrayAdapter<CharSequence> adpHorario = ArrayAdapter.createFromResource(this,
                 R.array.Horario, android.R.layout.simple_spinner_item);
 
-
+        spnInicio = findViewById(R.id.spnInicio);
         spnInicio.setOnItemSelectedListener(this);
         adpHorario.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spnInicio.setAdapter(adpHorario);
-        spnInicio.setOnItemSelectedListener(this);
 
 
+        spnFim = findViewById(R.id.spnFim);
         spnFim.setOnItemSelectedListener(this);
         adpHorario.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spnFim.setAdapter(adpHorario);
-        spnFim.setOnItemSelectedListener(this);
 
         FirebaseFirestore.getInstance().collection("/users")
                 .document(FirebaseAuth.getInstance().getUid())
@@ -246,14 +230,6 @@ public class PesquisaActivity extends AppCompatActivity implements AdapterView.O
                 break;
 
         }
-
-
-        switch(adapterView.getId()){
-            case 1:
-                Toast.makeText(this, "aquui", Toast.LENGTH_SHORT).show();
-
-        }
-
 
 
     }
